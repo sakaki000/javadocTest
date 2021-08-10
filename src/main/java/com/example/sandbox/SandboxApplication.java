a3886b5f70d0c18f36ec92bfbeadf5fb2bc50c14
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-08-10 17:04:02 +0900

    Add files via upload

---
+++ src/main/java/com/example/sandbox/SandboxApplication.java
@@ -0,0 +1,14 @@
+package com.example.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxApplication {
+
+	public static void main(String[] args) {
+		System.out.println("a");
+		SpringApplication.run(SandboxApplication.class, args);
+	}
+
+}
