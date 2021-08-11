c7afa9f159e2ccca69551b7cef390ef406c8889c
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-08-11 16:20:26 +0900

    Create exclude.java

---
+++ src/main/java/com/example/exclude/exclude.java
@@ -0,0 +1,31 @@
+package com.example.sandbox;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+
+    /***
+     * @param in inputinputinputinputinputinputinputinput
+     * @param out output 
+     * @return String String
+     */
+    public String get(String in, String out) {
+        return "Hello";
+    }
+
+
+    /**
+    *  コメントの説明文
+    * @return String String
+    *  @deprecated 別のメソッドに置き換えられました
+    */
+    public String out() {
+        return "dep";
+    }
+
+    public String useDep() {
+        return out();
+    }
+}
