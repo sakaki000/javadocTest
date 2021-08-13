4d313dad055bf6eb8eda9f92f56657d83dce9dc7
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-08-13 15:55:36 +0900

    Create test.js

---
+++ src/react/test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { FormApp } from './FormApp';
+
+/**
+ * これはテストです
+ */
+class App extends Component {
+  render() {
+    return (
+      <FormApp />
+    );
+  }
+}
+
+export default App;
