e86a8a32b8601a6fab588e4de65d36a160f7e1b6
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-08-27 22:31:47 +0900

    Update exclude.java

--- second/src/main/java/com/exclude/exclude.java
+++ second/src/main/java/com/exclude/exclude.java
@@ -7,8 +7,9 @@ public class SampleController {
 
 
     /***
-     * @param in main_copy
-     * @param j iii 
+     * @param in plus_main
+     * @param out output
+     * @return String String
      */
     public String get(String in, String out) {
         return "Hello";
