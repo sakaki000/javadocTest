5a33af202edc18e7646655a26d4f9c78f9787a1d
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-08-27 16:08:44 +0900

    Delete src directory

--- src/react/test.js
+++
@@ -1,9 +0,0 @@
-/**
- * Helloという文字列を返します。
- * @module hello
- * @param {string} name - 表示したい名前を指定する。
- * @return {string} - [Hello + name]という形式で戻る。
- */
-module.exports = hello = (name) => {
-    return "Hello" + name;
-}
