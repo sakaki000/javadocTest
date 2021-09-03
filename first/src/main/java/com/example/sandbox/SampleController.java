0008d447ed6597add4dc9ae814b7d38ad56df8ff
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-09-03 17:00:09 +0900

    Update SampleController.java

--- first/src/main/java/com/example/sandbox/SampleController.java
+++ first/src/main/java/com/example/sandbox/SampleController.java
@@ -7,9 +7,9 @@ public class SampleController {
 
 
     /***
-     * @param in PR_TEST2
-     * @param out PR_TEST2
-     * @return String PR_TEST2
+     * @param in PR_TEST3
+     * @param out PR_TEST3
+     * @return String PR_TEST3
      */
     public String get(String in, String out) {
         return "Hello";
