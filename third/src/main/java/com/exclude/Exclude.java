2dca923ef9e43844e8375bf9ae5970d1c15019fe
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-09-07 17:59:31 +0900

    Update Exclude.java

--- third/src/main/java/com/exclude/Exclude.java
+++ third/src/main/java/com/exclude/Exclude.java
@@ -8,7 +8,6 @@ public class Exclude {
 
     /***
      * @param in main_copy2
-     * @param out main_copy2
      * @return String main_copy2
      */
     public String get(String in, String out) {
@@ -19,7 +18,6 @@ public class Exclude {
     /**
     *  コメントの説明文
     *  @deprecated 別のメソッドに置き換えられました
-    *  @return String main_copy2
     */
     public String out() {
         return "dep";
