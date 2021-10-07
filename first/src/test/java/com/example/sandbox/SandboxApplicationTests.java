8603757f7fcae36a39947c35fdeab1e8cc1b7cf5
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-10-07 17:51:55 +0900

    Update SandboxApplicationTests.java

--- first/src/test/java/com/example/sandbox/SandboxApplicationTests.java
+++ first/src/test/java/com/example/sandbox/SandboxApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.sandbox;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SandboxApplicationTests {
     @Test
     void contextLoads() {
