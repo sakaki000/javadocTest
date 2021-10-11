7b6e95dbfa8dde7877f0b2e7eb92ca64503d8cc7
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-10-11 15:58:00 +0900

    Create ExcludeTest.java

---
+++ second/src/test/java/com/exclude/ExcludeTest.java
@@ -0,0 +1,14 @@
+package com.exclude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SandboxApplicationTests {
+    @Test
+    void contextLoads() {
+        Exclude  calc = new Exclude ();
+        assertEquals("dep", calc.Exclude("A"));
+    }
+}
