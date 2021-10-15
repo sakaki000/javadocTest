dc3b7af66eeb8dfa94c5e3d3e931d31520a03a09
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-10-15 18:19:23 +0900

    Update and rename third/src/test/java/com/example/sandbox/SandboxApplicationTests.java to third/src/test/java/com/exclude/SandboxApplicationTests.java

---
+++ third/src/test/java/com/exclude/SandboxApplicationTests.java
@@ -0,0 +1,25 @@
+package com.exclude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SandboxApplicationTests {
+    @Test
+    void contextLoads() {
+        Exclude calc = new Exclude("A");
+        assertEquals("A", calc.getName());
+    }
+    
+    @Test
+    void contextLoads2() {
+        Exclude calc = new Exclude("C");
+        assertEquals("C", calc.getName());
+    }
+
+    @Test
+    void contextLoads3() {
+        Exclude calc = new Exclude("B");
+        assertEquals("B", calc.getName());
+    }
+}
