c184a57163b86f431b4ba341215c0f708082857f
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-10-15 18:14:23 +0900

    Update ExcludeTest.java

--- second/src/test/java/com/exclude/ExcludeTest.java
+++ second/src/test/java/com/exclude/ExcludeTest.java
@@ -10,4 +10,16 @@ class SandboxApplicationTests {
         Exclude calc = new Exclude("A");
         assertEquals("A", calc.getName());
     }
+
+    @Test
+    void contextLoads2() {
+        Exclude calc = new Exclude("C");
+        assertEquals("C", calc.getName());
+    }
+
+    @Test
+    void contextLoads3() {
+        Exclude calc = new Exclude("B");
+        assertEquals("B", calc.getName());
+    }
 }
