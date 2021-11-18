03d436b63b8713a484e53cba4b751668fbe55eab
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-11-18 17:21:26 +0900

    Update and rename SandboxApplication.java to SandboxApplicationChageName.java

--- first/src/main/java/com/example/sandbox/SandboxApplicationChageName.java
+++ first/src/main/java/com/example/sandbox/SandboxApplicationChageName.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SandboxApplication {
+public class SandboxApplicationChageName {
 
 	public static void main(String[] args) {
 		System.out.println("a");
