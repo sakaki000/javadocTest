195b89d3e7bdb41ca670aa183e2031bd06822c0a
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-11-26 14:02:04 +0900

    Update and rename SandboxApplicationChangeName.java to SandboxApplication.java

--- first/src/main/java/com/example/sandbox/SandboxApplication.java
+++ first/src/main/java/com/example/sandbox/SandboxApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SandboxApplicationChangeName {
+public class SandboxApplication {
 
 	public static void main(String[] args) {
 		System.out.println("a");
-		SpringApplication.run(SandboxApplicationChangeName.class, args);
+		SpringApplication.run(SandboxApplication.class, args);
 	}
 
 	/**
