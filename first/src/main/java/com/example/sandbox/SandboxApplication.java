37023a749ddc62163421ca3565ed6b55e98a1bfe
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-12-21 17:45:44 +0900

    Update SandboxApplication.java

--- first/src/main/java/com/example/sandbox/SandboxApplication.java
+++ first/src/main/java/com/example/sandbox/SandboxApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SandboxApplication {
 
 	public static void main(String[] args) {
-		System.out.println("a");
+		System.out.println(out());
 		SpringApplication.run(SandboxApplication.class, args);
 	}
 
