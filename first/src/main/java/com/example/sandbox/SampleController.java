95e4547925a6e134980ec98787a82dd69d0708dc
sakaki000 <51310215+sakaki000@users.noreply.github.com>
2021-12-22 18:02:40 +0900

    Create SampleController.java

---
+++ first/src/main/java/com/example/sandbox/SampleController.java
@@ -0,0 +1,32 @@
+package com.example.sandbox;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+
+    /***
+     * @param in test 
+     * @param out test
+     * @return String test
+     */
+    public String get(String in, String out) {
+        return "Hello";
+    }
+
+
+    /**
+    *  コメントの説明文
+    *  @return String test
+    *  @deprecated 古い仕様なので、アクセスがなくなったら削除予定。
+    */
+    @Deprecated
+    public String out() {
+        return "dep";
+    }
+
+    public String useDep() {
+        return out();
+    }
+}
